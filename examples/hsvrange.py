eda2fc368f67f5cc103f01ef369e7e5b05209760
FRC830 <FRC830@users.noreply.github.com>
2016-08-02 18:10:22 -0400

    add bounding rectangle and width

--- examples/hsvrange.py
+++ examples/hsvrange.py
@@ -16,12 +16,16 @@ def on_frame(image, camera):
     mask = cv2.inRange(hsvimage, lower, upper)
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = contours[0]
     
     new_image = cv2.bitwise_and(orig_image, orig_image, mask=mask)
 
     #draw rectangle around largest contour
-    x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(new_image,(x,y),(x+w,y+h),(0,255,0),2)
+    if len(contours):
+        cnt = sorted(contours, key = cv2.contourArea, reverse = True)[:1][0]
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(new_image,(x,y),(x+w,y+h),(0,255,0),2)
+        #cv2.putText(new_image, "Width:%i"%w, (x,y), 
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(new_image,"Width:%i"%w,(x,y-5), font, 1,(0,0,255),2)
 
     return new_image
