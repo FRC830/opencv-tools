61d575aa1212fe02f52ce3aa97d2a0c97566aec2
lethosor <lethosor@gmail.com>
2017-04-30 21:53:41 -0400

    Use revokeObjectURL() appropriately

--- static/stream.js
+++ static/stream.js
@@ -1,8 +1,17 @@
 $(function() {
     socket = io('/stream/' + $('#stream-target').attr('data-ns'));
+    var image_url;
+    var URL = window.URL || window.webkitURL;
+    if (!URL) {
+        throw new Error("window.URL not available");
+    }
     socket.on('frame', function(data) {
         var blob = new Blob([new Uint8Array(data.raw)], {type: "image/jpeg"});
-        var imageUrl = (window.URL || window.webkitURL).createObjectURL(blob);
-        document.getElementById('stream-target').src = imageUrl;
-    })
+        // free the old URL
+        if (image_url) {
+            URL.revokeObjectURL(image_url);
+        }
+        image_url = URL.createObjectURL(blob);
+        document.getElementById('stream-target').src = image_url;
+    });
 });
